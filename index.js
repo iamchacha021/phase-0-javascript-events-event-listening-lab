b957f42511f23065607a8b032c720b965919012e
iamchacha021 <iamchacha021@gmail.com>
2022-12-27 15:38:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    let input=document.querySelector('#input')
+    input.addEventListener('click', clickFn)
+    function clickFn(){
+        alert('i have been clicked')
+    }
 }
+addingEventListener()
